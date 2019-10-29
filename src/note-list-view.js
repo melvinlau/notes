80701356dce2e02a628bc2f596eb86da4fd5e150
Melvin Lau <lau.melvin@icloud.com>
2019-10-29 18:04:03 +0000

    Passed a test for noteListView, not yet dealt with edge cases

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -2,5 +2,12 @@ class NoteListView {
   constructor(noteList) {
     this.noteList = noteList;
   }
-  
+  display() {
+    let html = '<ul>';
+    this.noteList.view().forEach((note) => {
+      html += `<li>${note}</li>`
+    });
+    html += '</ul>'
+    return html;
+  }
 }
