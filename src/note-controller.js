141ca21b3d8d4316573a5273cf88f5cd77ce76cc
Melvin Lau <lau.melvin@icloud.com>
2019-10-30 11:11:19 +0000

    Trial run to update div contents using innerHTML

---
+++ src/note-controller.js
@@ -0,0 +1,25 @@
+function sayHowdy() {
+  document.getElementById("app").innerHTML = "Howdy!";
+}
+
+document.onload = sayHowdy();
+
+
+
+
+
+
+
+
+
+
+// let note = new Note();
+
+// createButton = document.querySelector("#create");
+//
+// function createNote(note) {
+//   note.create(note);
+//   console.log(note.all());
+// }
+//
+// createButton.onclick = createNote('Test entry')
