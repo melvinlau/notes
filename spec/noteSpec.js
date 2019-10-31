93acba8e3f9be39df41bbef2ea439d324ba81860
Melvin Lau <lau.melvin@icloud.com>
2019-10-31 15:47:01 +0000

    Note model has unique id

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -9,3 +9,9 @@
   let note = new Note('Hello World');
   assert.isTrue(note.view() === 'Hello World');
 })();
+
+(function newNoteHasUniqueID() {
+  let note1 = new Note('I love coffee');
+  let note2 = new Note('I like hot chocolate');
+  assert.isTrue(note2.id === note1.id + 1);
+})();
--- src/note-model.js
+++ src/note-model.js
@@ -2,9 +2,21 @@
   class Note {
     constructor(text) {
       this.text = text;
+      this._id = Note.index;
+      Note.index += 1;
     }
     view() {
       return this.text;
+    }
+    get id() {
+      return this._id;
+    }
+    static get index() {
+      Note._index = (Note._index || 0);
+      return Note._index;
+    }
+    static set index(value) {
+      Note._index = value;
     }
   }
   exports.Note = Note;
